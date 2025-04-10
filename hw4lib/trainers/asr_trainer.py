84945ba1339051e1000c80a41cbd9525983673ad
BrOccoli716 <124345449+BrOccoli716@users.noreply.github.com>
2025-04-09 22:24:57 -0400

    Update asr_trainer.py

--- hw4lib/trainers/asr_trainer.py
+++ hw4lib/trainers/asr_trainer.py
@@ -62,7 +62,7 @@ class ASRTrainer(BaseTrainer):
         # TODO: Initialize CE loss
         # How would you set the ignore_index? 
         # Use value in config to set the label_smoothing argument
-        self.ce_criterion = nn.CrossEntropyLoss(ignore_index=self.tokenizer.pad_id, label_smoothing=self.config['loss'].get('label_smoothing', 0.0)
+        self.ce_criterion = nn.CrossEntropyLoss(ignore_index=self.tokenizer.pad_id, label_smoothing=self.config['loss'].get('label_smoothing', 0.0))
         
         # TODO: Initialize CTC loss if needed
         # You can use the pad token id as the blank index
